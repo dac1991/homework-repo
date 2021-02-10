256babdb26b0ab4e0cec710dd3502997494fb5c2
dac1991 <dominiccalgie1991@gmail.com>
2021-02-10 22:45:30 +0000

    Homework Commit

---
+++ hw_traveller/hw_traveller_start/models/traveller.js
@@ -0,0 +1,49 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+
+  });
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+
+  });
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  });
+
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance;
+  });
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((runningTotal, journey) => {
+    return runningTotal + journey.distance;
+  }, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  const transports = this.journeys.map((journey) => {
+    return journey.transport;
+  });
+
+  return transports.filter((value, index, array) => {
+    return array.indexOf(value) === index;
+  });
+};
+
+
+module.exports = Traveller;
